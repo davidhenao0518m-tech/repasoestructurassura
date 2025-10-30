9774566d1cde3c6134eef38392a89cc007386dbf
diseño y desarrollo <disenoydesarrollo@academicos.cesde.edu.co>
2025-10-30 16:24:09 -0500

    feat se crea inicio de la app configurando mensaje inicio con colores

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,54 @@
 package org.example;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Comienza aca: ");
+        //System.out.println("Comienza aca: ");
+
+        Scanner leerTeclado=new Scanner(System.in);
+        String colorNegro="\u001B[30m";
+        String colorVerde="\u001B[35m";
+        String colorVerde="\u001B[35m";
+        String colorVerde="\u001B[35m";
+        String colorVerde="\u001B[35m";
+        String colorVerde="\u001B[35m";
+
+        String correoDigitalUusario;
+        String contraseñaDigitaLUsuario;
+        String correoGuardadoBaseDatos="messi10@gmail.com";
+        String contraseñaGuardadaBaseDatos="messielmejor";
+
+
+
+        //PROCESO
+        System.out.println(colorVerde+"*******************");
+        System.out.println("GESTOR APP by JJG 2025 🤡🤡🤡🤡");
+        System.out.println("*******************");
+
+
+        //Diseñar un ALGORITMO que permita el ingreso
+        //de un usuario con correo y contraseña
+        //se permitira 3 intentos antes de bloquearlo
+
+        //Diseñar un ALGORITMO que permita mostrar un menu de opciones
+        //si el usuario se auntentico con exito, el menu sera el siguitente:
+
+        //1. registrar venta
+        //2. mostrar ventas al dia
+        //3. salir
+
+        //NOTA:una venta se considera en el sistema como una variable
+        //con los siguientes datos
+        //id-->LocalDate
+        //descripcion  -->cadena
+        //listas de productos --lista
+        //total --> double
+
+        //nota2: un producto tiene
 
 
     }
